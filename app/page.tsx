6ae4842ed8226435295f42969c42bb4b6b310fc3
PromiseUdo <info.promiseudo@gmail.com>
2025-05-03 17:54:32 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -9,26 +9,26 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    // <LoaderWrapper>
-    <main className="bg-background text-foreground">
-      <MaxWidthWrapper className="py-16 md:py-20">
-        <div className="flex flex-col space-y-12">
-          <div className="flex flex-col space-y-6">
-            <h1 className="text-5xl  ">
-              Your Digital Future, Powered by Innovation.
-            </h1>
-            <p className="text-base text-white max-w-2xl">
-              We deliver cutting-edge development, stunning design, and robust
-              cybersecurity solutions.
-            </p>
+    <LoaderWrapper>
+      <main className="bg-background text-foreground">
+        <MaxWidthWrapper className="py-16 md:py-20">
+          <div className="flex flex-col space-y-12">
+            <div className="flex flex-col space-y-6">
+              <h1 className="text-5xl  ">
+                Your Digital Future, Powered by Innovation.
+              </h1>
+              <p className="text-base text-white max-w-2xl">
+                We deliver cutting-edge development, stunning design, and robust
+                cybersecurity solutions.
+              </p>
+            </div>
+            <OurProducts />
+            <Services />
+            <BlogServer />
+            <Connect />
           </div>
-          <OurProducts />
-          <Services />
-          <BlogServer />
-          <Connect />
-        </div>
-      </MaxWidthWrapper>
-    </main>
-    // </LoaderWrapper>
+        </MaxWidthWrapper>
+      </main>
+    </LoaderWrapper>
   );
 }
